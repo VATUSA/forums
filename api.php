94d11824bc4d3bd0ed673c3f02c2a37f30be6eaa
Daniel Hawton <daniel@hawton.com>
2017-12-24 17:29:25 -0900

    Add ULS API

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+
+require("./smf_2_api.php");
+
+if ($_REQUEST['login']) {
+  $token = $_REQUEST['token'];
+  $signature = $_REQUEST['signature'];
+  if ($signature != hash_hmac("sha512", $signature, file_get_contents("/home/vatusa/forum.key")) {
+    echo "Bad token\n"; exit;
+  }
+
+  $data = json_decode($token, true);
+  if (!$data['nlt'] || $data['nlt'] > time()) {
+    echo "Expired token\n"; exit;
+  }
+
+  smfapi_login($data['cid']);
+}
