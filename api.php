25d5e870ed8e0907c158d309ff76618e04227a6c
Daniel Hawton <daniel@hawton.com>
2017-12-24 17:31:02 -0900

    Use base64 decode

--- api.php
+++ api.php
@@ -5,7 +5,7 @@ require("./smf_2_api.php");
 if ($_REQUEST['login']) {
   $token = $_REQUEST['token'];
   $signature = $_REQUEST['signature'];
-  if ($signature != hash_hmac("sha512", $signature, file_get_contents("/home/vatusa/forum.key")) {
+  if ($signature != hash_hmac("sha512", $signature, base64_decode(file_get_contents("/home/vatusa/forum.key"))) {
     echo "Bad token\n"; exit;
   }
 
